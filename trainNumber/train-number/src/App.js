b115f597c89e3ad505d4376ad3ecfb1341f79a0e
Simplisticbubble <ks.kevinseu@gmail.com>
2023-11-17 10:52:26 +1100

    Added basic Structure

---
+++ trainNumber/train-number/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {Calculator, Station, Train} from './container';
+function App() {
+  return (
+    <div>
+      <Station/>
+      <Train/>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
