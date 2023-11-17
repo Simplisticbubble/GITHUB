2a1cd058976a0ebe7d764823ddf7108382a94143
Simplisticbubble <ks.kevinseu@gmail.com>
2023-11-17 15:47:40 +1100

    Added main function of the UI

--- trainNumber/train-number/src/container/Calculator/Calculator.jsx
+++ trainNumber/train-number/src/container/Calculator/Calculator.jsx
@@ -1,15 +1,67 @@
 import React, {useState} from "react";
 
 const Calculator = () => {
-    const [Index, setIndex] = useState(0);
-    const handleIndex = () => {
-        setIndex((prevIndex)=> prevIndex + 1)
-    }
+    const num1 = 7;
+    const num2 = 2;
+    const num3 = 2;
+    const num4 = 1;
+    const [divs, setDivs] = useState([num1, num2, num3, num4]);
+    const [displayValue, setDisplayValue] = useState(''); // State to track the displayed value
+    const [currentValue, setCurrentValue] = useState(''); // State to store the current value for calculation
+
+    const [operator, setOperator] = useState(null);// State to store the current value
+
+    // Function to handle number clicks
+  const handleNumberClick = (number) => {
+    setDisplayValue((prevDisplayValue) => prevDisplayValue + number);
+  };
+
+    // Function to handle operator clicks
+    const handleOperatorClick = (op) => {
+        setCurrentValue(displayValue);
+        setDisplayValue('');
+        setOperator(op);
+      };
+      const handleClearClick = () => {
+        setDisplayValue('');
+        setCurrentValue('');
+        setOperator(null);
+      };
+      const handleImpossibleClick = () => {
+        setDisplayValue('');
+        setCurrentValue('');
+        setOperator(null);
+      };
+
     return (
-    <div className="app__Station">
-        <div className="app__Station-container">
-            <h1 className="app__Station-name">Calculator</h1>
-        </div>
+        <div className="app__Calculator">
+            <div className="app__Calculator-display">
+                <div className="app__Calculator-digit">
+                    
+                </div>
+                <div className="app__Calculator-digit">
+                    
+                </div>
+                <div className="app__Calculator-digit">
+                    
+                </div>
+                <div className="app__Calculator-digit">
+                    
+                </div>
+            </div>
+{/***************************************************************************************************************************************************** */}
+            <div className="app__Calculator-keypad">
+                <div className="buttons-grid">
+                    <button onClick={() => handleOperatorClick('/')}>/</button>
+                    <button onClick={() => handleOperatorClick('*')}>*</button>
+                    <button onClick={() => handleOperatorClick('-')}>-</button>
+                    <button onClick={() => handleOperatorClick('+')}>+</button>
+                </div>
+                <div className="buttons-grid-special">
+                    <button onClick={handleClearClick}>C</button>
+                    <button onClick={handleImpossibleClick}>Not Possible</button>
+                </div>
+            </div>
     </div>
 );};
 
